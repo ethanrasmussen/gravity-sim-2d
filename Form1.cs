bafbdcdbab9cc9d6a1eef9c2887d2ff148e5a7f6
Ethan Rasmussen <ethanmr3@illinois.edu>
2025-05-25 11:36:57 -0500

    Double buffer

--- Form1.cs
+++ Form1.cs
@@ -5,5 +5,12 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+
+        // enable double buffering:
+        SetStyle(ControlStyles.AllPaintingInWmPaint | 
+                ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.UserPaint, true);
+        UpdateStyles();
+
     }
 }
